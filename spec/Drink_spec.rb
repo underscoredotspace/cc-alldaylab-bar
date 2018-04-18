eeb5ea1e1eb03878105ff7948f95ef91ea071786
Colin Tindle <ampersand@underscore.space>
2018-04-18 11:04:14 +0100

    created drink class

--- spec/Drink_spec.rb
+++ spec/Drink_spec.rb
@@ -4,6 +4,14 @@ require_relative('../Drink')
 
 class TestDrink < MiniTest::Test
   def setup
-    
+    @drink = Drink.new('Beer', 3)
+  end
+
+  def test_drink_has_name
+    assert_equal('Beer', @drink.name)
+  end
+
+  def test_drink_has_price
+    assert_equal(3, @drink.price)
   end
 end
