6f28485d9c49ccd6a5e47dd685a25aad01b3923c
Colin Tindle <ampersand@underscore.space>
2018-04-18 15:29:04 +0100

    Drink now has alcohol_level, Customer has drunkeness which increases by alcohol_level when they buy drink

--- Customer.rb
+++ Customer.rb
@@ -1,14 +1,18 @@
 class Customer
-  attr_reader(:name, :wallet, :age)
+  attr_reader(:name, :wallet, :age, :drunkenness)
 
   def initialize(name, wallet, age)
     @name = name
     @wallet = wallet
     @age = age
+    @drunkenness = 0
   end
 
   def buy_a_drink(bar, beverage)
     drink_sold = bar.sell_a_drink(beverage, @age)
-    @wallet -= beverage.price if drink_sold
+    if drink_sold
+      @wallet -= beverage.price
+      @drunkenness += beverage.alcohol_level
+    end
   end
 end
--- spec/Customer_spec.rb
+++ spec/Customer_spec.rb
@@ -9,9 +9,9 @@ class TestCustomer < MiniTest::Test
     @customer = Customer.new('Annie', 100, 35)
     @customer2 = Customer.new('John', 10, 16)
 
-    @beer = Drink.new('Beer', 3)
-    @wine = Drink.new('Wine', 4)
-    @rum = Drink.new('Rum', 6)
+    @beer = Drink.new('Beer', 3, 1)
+    @wine = Drink.new('Wine', 4, 2)
+    @rum = Drink.new('Rum', 6, 4)
     drinks = [@beer, @wine, @rum]
     
     @pub = Pub.new("The Bear arms", 0, drinks)
@@ -39,6 +39,16 @@ class TestCustomer < MiniTest::Test
     @customer2.buy_a_drink(@pub, @rum)
     assert_equal(10, @customer2.wallet)
     assert_equal(0, @pub.till)
+    assert_equal(0, @customer2.drunkenness)
   end
   
+  def test_customer_has_drunkenness_level
+    assert_equal(0, @customer.drunkenness)
+  end
+
+  def test_customer_gets_drunker
+    @customer.buy_a_drink(@pub, @wine)
+    assert_equal(2, @customer.drunkenness)
+  end
+
 end
