d698f8e4b358852122899fa950b91cea3e5c74d8
Colin Tindle <ampersand@underscore.space>
2018-04-18 11:09:58 +0100

    Created Customer class

--- spec/Customer_spec.rb
+++ spec/Customer_spec.rb
@@ -4,6 +4,15 @@ require_relative('../Customer')
 
 class TestCustomer < MiniTest::Test
   def setup
-    
+    @customer = Customer.new('Annie', 100)
   end
+
+  def test_customer_has_name
+    assert_equal('Annie', @customer.name)
+  end
+
+  def test_customer_has_wallet
+    assert_equal(100, @customer.wallet)
+  end
+  
 end
