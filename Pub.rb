7ee112ae9fc9a6aa2bdecc95bfdaa331a51de140
Colin Tindle <ampersand@underscore.space>
2018-04-18 11:23:29 +0100

    Created pub class

---
+++ Pub.rb
@@ -0,0 +1,14 @@
+class Pub
+
+  attr_reader :name, :till, :drinks
+
+  def initialize(name,till)
+    @name = name
+    @till = till
+    @drinks = []
+  end
+
+
+
+
+end
--- spec/Pub_spec.rb
+++ spec/Pub_spec.rb
@@ -4,6 +4,18 @@ require_relative('../Pub')
 
 class TestPub < MiniTest::Test
   def setup
-    
+    @pub = Pub.new("The Bear arms", 0)
+  end
+
+  def test_pub_has_name
+    assert_equal('The Bear arms', @pub.name)
+  end
+
+  def test_pub_has_empty_till
+    assert_equal(0, @pub.till)
+  end
+
+  def test_pub_has_no_drinks
+    assert_equal([], @pub.drinks)
   end
 end
